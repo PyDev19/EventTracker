39143615f62615063ed1ae1866a9d404f9a9a76e
Atharva Mishra <atharvamishra956+1@gmail.com>
2024-01-04 10:45:28 -0600

    add firebase context to qml engine

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,30 @@
 #include <QGuiApplication>
+#include <QJniEnvironment>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <qnativeinterface.h>
+#include <firebase/app.h>
+#include "firebase_auth.hpp"
+
+using namespace QNativeInterface;
+using firebase::App;
 
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
+    app.setApplicationName("EventTracker");
+
+    QJniEnvironment *jni_env = new QJniEnvironment();
+    App* firebase_app = App::Create(AppOptions(), jni_env->jniEnv(), QAndroidApplication::context());
 
     QQmlApplicationEngine engine;
+    
+    FirebaseAuth firebase_auth(firebase_app);
+    engine.rootContext()->setContextProperty("auth", &firebase_auth);
+
     const QUrl url(u"qrc:/EventTracker/Main.qml"_qs);
     QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
+        &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
+        []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
     engine.load(url);
 
     return app.exec();
